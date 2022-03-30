03c79aeb745c7544d8f884808223fcc03abf8c81
Tingwei Shen <tingweishen@berkeley.edu>
2022-03-29 20:32:10 -0700

    added 1d 2d nd sampling files

---
+++ 1dsampling.jl
@@ -0,0 +1,89 @@
+using Surrogates
+using Plots
+default()
+
+# Define salustowicz function
+function salustowicz(x)
+    term1 = 2.72^(-x) * x^3 * cos(x) * sin(x);
+    term2 = (cos(x) * sin(x)*sin(x) - 1);
+    y = term1 * term2;
+end
+
+# Define gramacylee function
+function gramacylee(x)
+    term1 = sin(10*pi*x) / 2*x;
+    term2 = (x - 1)^4;
+    y = term1 + term2;
+end
+
+n_samples = 40      # number of sampling points
+lower_bound = -0.5
+upper_bound = 2.5
+num_round = 2
+
+# x = AbstractFloat[i*2 for i in 0:5]
+x = AbstractFloat[-0.5, 0, 0.5, 1.5, 2, 2.5]    # Set initial sampling points
+
+for sample_iter in 1:n_samples-length(x)
+    tempx = copy(x)     # Make a copy of current sampled points
+    # tempy = salustowicz.(tempx)
+    tempy = gramacylee.(tempx)      # Calculate the true values of sampled points
+
+    # function that calcualtes the cv error of a potential point
+    function cv_error(new_sample_point_x)
+        norm = 0
+        radial_surrogate = RadialBasis(tempx, tempy, lower_bound, upper_bound)  # find the surrogate function using current sampled points
+        
+        # for each sampled point, we find the leave-one-out surrogate function
+        for sampled_point in 1:length(tempx)
+            loo_x = copy(tempx)
+            loo_y = copy(tempy)
+            deleteat!(loo_x, sampled_point)
+            deleteat!(loo_y, sampled_point)
+            loo_radial_surrogate = RadialBasis(loo_x, loo_y, lower_bound, upper_bound)  # find the surrogate function that leaves one sampled point out
+            norm = norm + (loo_radial_surrogate(new_sample_point_x) - radial_surrogate(new_sample_point_x))^2   # Add up the norm to calculate cv error
+        end
+        e = (norm / length(tempx))^(1/2)    # Return the cv error by mean squaring the norm
+    end
+
+    # function that returns the minimum distance between the input point and one of the already sampled points
+    function min_distance(new_sample_point_x)
+        sample_point_x_arr = fill(new_sample_point_x, length(tempx))
+        distance_arr = broadcast(-, sample_point_x_arr, tempx)
+        distance_arr = broadcast(abs, distance_arr)
+        d = minimum(distance_arr)
+    end
+
+    new_sample_point = 0
+    max_opt = 0     # number to maximize (cv_error * minimum_distance)
+    for target_sample_x in lower_bound:0.01:upper_bound
+        if target_sample_x in tempx
+            continue        # skip the points already sampled
+        else
+            
+            opt = cv_error(target_sample_x) * min_distance(target_sample_x)     # Calculate the output of optimization function for the point
+            if opt > max_opt
+                max_opt = opt
+                new_sample_point = target_sample_x      # Update the new sampling point as the point with biggest max_opt
+            end
+        end
+    end
+
+    print(new_sample_point)
+    print("  ")
+
+    push!(x, new_sample_point)      # Add the new sampled point into the sampling points list
+end
+
+
+# y = salustowicz.(x)
+y = gramacylee.(x)  # find the true values of all sampling points
+
+radial_surrogate = RadialBasis(x, y, lower_bound, upper_bound)  # find the surrogate function with all sampling points
+
+# Graph the true function and the surrogate function using out sampled points
+xs = lower_bound:0.001:upper_bound
+scatter(x, y, label="Sampled points", xlims=(lower_bound, upper_bound), legend=:top)
+# plot!(xs, salustowicz.(xs), label="True function", legend=:top)
+plot!(xs, gramacylee.(xs), label="True function", legend=:top)
+plot!(radial_surrogate, label="Surrogate function",  xlims=(lower_bound, upper_bound), legend=:top)
---
+++ 2dsampling.jl
@@ -0,0 +1,125 @@
+# Run:
+# import Pkg; Pkg.add("Surrogates")
+# import Pkg; Pkg.add("PolyChaos")
+# import Pkg; Pkg.add("Plots")
+using Surrogates
+using PolyChaos
+using Plots
+using LinearAlgebra
+default()
+
+# Define the 2d Rosenbrock function
+function Rosenbrock2d(x)
+    x1 = x[1]
+    x2 = x[2]
+    return (1-x1)^2 + 100*(x2-x1^2)^2
+end
+
+
+n = 100
+lb = [0.0,0.0]
+ub = [8.0,8.0]
+initial_n = 17
+
+xys = sample(initial_n,lb,ub,SobolSample());
+push!(xys, (0.0, 0.0))
+push!(xys, (0.0, 8.0))
+push!(xys, (8.0, 0.0))
+push!(xys, (8.0, 8.0))
+zs = Rosenbrock2d.(xys);
+
+xs = [xy[1] for xy in xys]
+ys = [xy[2] for xy in xys]
+ps = surface(x, y, (x, y) -> poly_surrogate([x y]), title="Initial Polynomial expansion")
+scatter!(xs, ys, zs, marker_z=zs)
+pc = contour(x, y, (x, y) -> poly_surrogate([x y]), title="Initial Polynomial expansion")
+scatter!(xs, ys, marker_z=zs)
+display(plot(ps, pc))
+
+
+for sample_iter in 1:(n-initial_n-4)
+    curr_sampled_n = length(xys)
+    print(curr_sampled_n)
+    if curr_sampled_n % 20 == 0
+        tempzs = Rosenbrock2d.(xys);
+        tempxs = [xy[1] for xy in xys]
+        tempys = [xy[2] for xy in xys]
+        tempps = surface(x, y, (x, y) -> poly_surrogate([x y]), title="$curr_sampled_n Points Polynomial expansion")
+        scatter!(tempxs, tempys, tempzs, marker_z=tempzs)
+        temppc = contour(x, y, (x, y) -> poly_surrogate([x y]), title="$curr_sampled_n Points Polynomial expansion")
+        scatter!(tempxs, tempys, marker_z=tempzs)
+        display(plot(tempps, temppc, size = (1000, 800)))
+    end
+    tempxys = copy(xys)
+    tempzs = Rosenbrock2d.(tempxys)
+
+    function cv_error(new_sample_point_x)
+        norm = 0
+        poly_surrogate = PolynomialChaosSurrogate(tempxys, tempzs, lb, ub)
+    
+        for sampled_point in tempxys
+            loo_xys = copy(tempxys)
+            loo_zs = copy(tempzs)
+            deleteat!(loo_xys, findall(x->x==sampled_point,loo_xys))
+            deleteat!(loo_zs, findall(x->x==Rosenbrock2d(sampled_point),loo_zs))
+            loo_poly_surrogate = PolynomialChaosSurrogate(loo_xys, loo_zs, lb, ub)
+            norm = norm + (loo_poly_surrogate(new_sample_point_x) - poly_surrogate(new_sample_point_x))^2
+        end
+        e = (norm / length(tempxys))^(1/2)
+    end
+
+
+    function min_distance(new_sample_point_x)
+        arr_xys = [collect(i) for i in tempxys]
+        sample_point_x_arr = fill(new_sample_point_x, length(tempxys))
+        distance_arr = broadcast(-, sample_point_x_arr, arr_xys)
+        distance_arr = broadcast(norm, distance_arr)
+        d = minimum(distance_arr)
+    end
+
+    new_sample_point = (0.0, 0.0)
+    max_opt = 0
+    for target_sample_xys in sample(n,lb,ub,UniformSample())
+        if target_sample_xys in tempxys
+            continue
+        else
+            arr_target_sample_xys = [i for i in target_sample_xys]
+            opt = cv_error(arr_target_sample_xys) * min_distance(arr_target_sample_xys)
+            if opt > max_opt
+                max_opt = opt
+                new_sample_point = target_sample_xys
+            end
+        end
+    end
+
+    print(new_sample_point)
+    print("  ")
+
+    push!(xys, new_sample_point)
+end
+
+
+
+
+zs = Rosenbrock2d.(xys);
+poly_surrogate = PolynomialChaosSurrogate(xys, zs, lb, ub)
+
+
+true_xys = sample(n,lb,ub,SobolSample());
+true_zs = Rosenbrock2d.(true_xys);
+x, y = 0:8, 0:8
+p1 = surface(x, y, (x1,x2) -> Rosenbrock2d((x1,x2)), title="True function")
+true_xs = [xy[1] for xy in true_xys]
+true_ys = [xy[2] for xy in true_xys]
+p2 = contour(x, y, (x1,x2) -> Rosenbrock2d((x1,x2)), title="True function")
+scatter!(true_xs, true_ys)
+
+
+xs = [xy[1] for xy in xys]
+ys = [xy[2] for xy in xys]
+p3 = surface(x, y, (x, y) -> poly_surrogate([x y]), title="Polynomial expansion")
+scatter!(xs, ys, zs, marker_z=zs)
+p4 = contour(x, y, (x, y) -> poly_surrogate([x y]), title="Polynomial expansion")
+scatter!(xs, ys, marker_z=zs)
+display(plot!(p1, p2, p3, p4, size=(1000,800), reuse=false))
+# display(plot!(p1, ps, p3, p2, pc, p4, size=(1400,800), reuse=false))
